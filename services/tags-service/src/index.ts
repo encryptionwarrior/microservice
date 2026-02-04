aead33b7e8e60d14e9dad5d424f740ebe4cfcb9f
creativeSandeepKumar <sdp4209@gmail.com>
2026-02-05 01:35:48 +0530

    working on tags service

---
+++ services/tags-service/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import helment from "helmet";
+import dotenv from "dotenv";
+import { corsOptions, errorHandler, healthCheck } from "@shared/middleware";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3004;
+
+app.use(cors(corsOptions()))
+app.use(helment());
+
+app.use("/health", healthCheck);
+
+app.use(errorHandler);
+
+const server = app.listen(PORT, async () => {
+  console.log(`Tags service is running on port ${PORT}`);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+  console.log(`Health check: http://localhost:${PORT}/health`);
+});
+
+const garcefulShutdown = async () => {
+  console.log("🚦 Shutting down Tags Service...");
+  server.close(() => {
+    console.log("✅ Tags Service shut down gracefully");
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", garcefulShutdown);
+process.on("SIGTERM", garcefulShutdown);
+
+export default app;
