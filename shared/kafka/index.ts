c958ccf6530a9f6b79d20dca296287effd908cef
creativeSandeepKumar <sdp4209@gmail.com>
2026-02-08 17:41:48 +0530

    index is completed

--- shared/kafka/index.ts
+++ shared/kafka/index.ts
@@ -4,6 +4,6 @@ export * from "./config";
 
 export { KafkaProducer, getKafkaProducer, publishEvent } from "./producer";
 
-export {} from "./consumer";
+export { KafkaConsumer, createConsumer } from "./consumer";
 
 export type { RecordMetadata } from "kafkajs";
